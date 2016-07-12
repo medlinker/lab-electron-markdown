cacabb4882ab28044e768c96c279411624e79e8e
zhangmingrui <592044573@qq.com>
2016-07-12 19:03:14 +0800

    添加jquery,方便代码编写

--- renderer-process/blog/write.js
+++ renderer-process/blog/write.js
@@ -3,17 +3,17 @@
  * @version 1.0.0 | 2016-07-12 版本信息
  * @author Zhang Mingrui | 592044573@qq.com
  * */
-
+const $ = require('jquery');
 const ipc = require('electron').ipcRenderer;
 
-const asyncMsgBtn = document.getElementById('js-write-ipc'),
-    msgNode = document.getElementById('js-write-msg');
+const asyncMsgBtn = $('#js-write-ipc'),
+    msgNode = $('#js-write-msg');
 
-asyncMsgBtn.addEventListener('click', function () {
+asyncMsgBtn.on('click', function () {
   ipc.send('asynchronous-message', 'ping')
 });
 
 ipc.on('asynchronous-reply', function (event, arg) {
   var message = `Asynchronous message reply: ${arg}`
-  msgNode.innerHTML = message;
+  msgNode.html(message);
 });
